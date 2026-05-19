343278dff9ef2b2a781b59f6c5f15bb90a704993
katttiger <katttiger@gmail.com>
2026-05-19 13:24:42 +0200

    added SecurityConfig, User-entity with validation and dto

---
+++ src/main/java/se/iths/cecilia/authservice/entity/User.java
@@ -0,0 +1,29 @@
+package se.iths.cecilia.authservice.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Your username must be an email")
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
