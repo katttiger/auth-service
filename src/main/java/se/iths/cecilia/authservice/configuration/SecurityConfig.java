0ebdd5b87cfb7488d5ba5059d468e9f0b4de931b
katttiger <katttiger@gmail.com>
2026-09-22 10:14:35 +0200

    Added cors-config in SecurityConfig.

--- src/main/java/se/iths/cecilia/authservice/configuration/SecurityConfig.java
+++ src/main/java/se/iths/cecilia/authservice/configuration/SecurityConfig.java
@@ -22,6 +22,8 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.util.StringUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import se.iths.cecilia.authservice.entity.User;
 import se.iths.cecilia.authservice.repository.UserRepository;
 
@@ -33,6 +35,7 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Base64;
 
 @Configuration
@@ -43,6 +46,16 @@ public class SecurityConfig {
     private final String jwtPublicKey;
     private final String jwtPrivateKey;
     private final String jwtKeyId;
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://localhost:8080", "https://localhost:9000", "https://localhost:7500", "https://localhost:8083"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     public SecurityConfig(
             UserRepository userRepository,
@@ -101,7 +114,8 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService() {
         return username -> {
             User user = userRepository.findByUsername(username)
-                    .orElseThrow(() -> new UsernameNotFoundException("User with username " + username + " not found"));
+                    .orElseThrow(() -> new UsernameNotFoundException(
+                            "User with username " + username + " not found"));
             return new CustomUserDetails(user);
         };
     }
